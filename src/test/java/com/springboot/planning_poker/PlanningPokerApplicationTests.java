d8df593d3a3bc80a0ac24446ab0bda2d09a24858
T.T.DAT <55250111+ttdat-thecodeguy@users.noreply.github.com>
2022-04-12 14:36:46 +0700

    init project

---
+++ src/test/java/com/springboot/planning_poker/PlanningPokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.planning_poker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanningPokerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
